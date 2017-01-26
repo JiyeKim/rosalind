412c3f6e9b307dae908a96c22594cd4d33de62a8
Sinceritary Jihyun <SinceritaryJihyun@LeeJihyunui-MacBook.local>
2017-01-26 09:45:30 +0900

    final modified

--- fibd.py
+++ fibd.py
@@ -1,18 +1,21 @@
 import sys
 
 # ages
-print('input n month')
+print('input n month remain')
 n = int(sys.stdin.readline())
 # alive
-print('input m month')
+print('input m month alive')
 m= int(sys.stdin.readline())
-print('remain n month= ', n)
+print('remain n month = ', n)
 print('alive m month =', m)
 ages = [1] + [0]*(m-1)
-print (1, 'month later', ages)
+#print (1, 'month later', ages)
 for i in range(n-1):
   ages = [sum(ages[1:])] + ages[:-1]
   #print(i+2, 'month later', ages)
 
-print(sum(ages))
+print('\n','FIBD solution') 
+print(n, 'month later', ages)
+print('total number of rabbits =', sum(ages))
+
 
